aa24326613bf52b573d628896e864d3edd21bb3b
ekmerline <emilykmerline@gmail.com>
2021-03-09 16:17:57 +0000

    Wrote word count code

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,19 @@
 # put your code here.
+f = open("test.txt")
+
+words = f.read()
+
+f.close()
+
+counted_words = {}
+
+for word in words.split():
+    if word in counted_words:
+        counted_words[word] += 1
+    else:
+        counted_words[word] = 1
+
+
+
+for key, value in counted_words.items():
+    print(key, value)
